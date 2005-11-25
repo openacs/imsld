c4a8eb965a91a8ee33b8329e61f614a294690f01
Eduardo Pérez Ureta <eperez@it.uc3m.es>
2005-11-25 08:58:56 +0000

    leaving some working code for testing

--- www/index.adp
+++ www/index.adp
@@ -3,15 +3,9 @@
   <property name="context">@context;noquote@</property>
 
 <blockquote>
-multiple na"imslds_in_class">
-li><strong>imslds_in_class.imsld_title@</strong><br />
-    include src="../lib/imsld-chunk" imsld_item_id="imslds_in_class.imsld_id@">
-br /><br />
-/multiple>
+<multiple name="imslds_in_class">
+<li><strong>@imslds_in_class.imsld_title@</strong><br />
+    <include src="../lib/imsld-chunk" imsld_item_id="@imslds_in_class.imsld_id@">
+<br />
+</multiple>
 </bloquote>
-
-@listin1@
-<br>
-@listin2@
-<br>
-@listin3@
--- www/index.tcl
+++ www/index.tcl
@@ -5,7 +5,16 @@ set page_title index
 set context {}
 set community_id [dotlrn_community::get_community_id]
 
-
-
-# under construction
-
+db_multirow imslds_in_class get_manifests {
+    select cr3.item_id as imsld_id,
+    coalesce(imsld.title, imsld.identifier) as imsld_title
+    from acs_rels ar, cr_items cr1, cr_items cr2, cr_items cr3, imsld_cp_manifests icm, imsld_cp_organizations ico, imsld_imsldsi imsld 
+    where ar.object_id_one = :community_id
+    and ar.rel_type = 'imsld_community_manifest_rel'
+    and ar.object_id_two = cr1.item_id
+    and cr1.live_revision = icm.manifest_id
+    and ico.manifest_id = cr1.item_id
+    and imsld.organization_id = cr2.item_id
+    and cr2.live_revision = ico.organization_id
+    and cr3.live_revision = imsld.imsld_id
+}
