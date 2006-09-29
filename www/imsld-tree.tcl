6ae0d611eb794930c076fa3d23bb02f1705a1912
Jose Pablo Escobedo Del Cid <jose.escobedo@int-edu.eu>
2006-09-29 10:28:03 +0000

    setting automatically the current_role of the user if there is only one choice. internationalizing.

--- www/imsld-tree.tcl
+++ www/imsld-tree.tcl
@@ -24,6 +24,13 @@ db_1row imslds_in_class {
 
 # current role information.
 # the user must have an active role in the run
+set possible_user_roles [imsld::roles::get_user_roles -user_id $user_id -run_id $run_id]
+set possible_user_role_names [imsld::roles::get_roles_names -roles_list $possible_user_roles]
+# remove &nbsp; added in the previous proc
+regsub -all "&nbsp;" $possible_user_role_names "" $possible_user_role_names
+
+# if there is only one role, set it
+set current_role_id [expr { [llength $possible_user_roles] == 1 ? [lindex $possible_user_roles 0] : "" }]
 
 if { ![empty_string_p $current_role_id] } {
     # a role has been selected, update in db
@@ -38,11 +45,6 @@ if { ![empty_string_p $current_role_id] } {
     }
 }
 
-set possible_user_roles [imsld::roles::get_user_roles -user_id $user_id -run_id $run_id]
-set possible_user_role_names [imsld::roles::get_roles_names -roles_list $possible_user_roles]
-# remove &nbsp; added in the previous proc
-regsub -all "&nbsp;" $possible_user_role_names "" $possible_user_role_names
-
 if { ![db_0or1row get_current_role {
     select map.active_role_id as user_role_id
     from imsld_run_users_group_rels map,
@@ -52,10 +54,11 @@ if { ![db_0or1row get_current_role {
     and ar.object_id_one = iruge.group_id
     and ar.object_id_two = :user_id
     and iruge.run_id = :run_id
+    and map.active_role_id is not null
 }] } {
     # generate the first option
     set possible_user_roles [linsert $possible_user_roles 0 0]
-    set possible_user_role_names [linsert $possible_user_role_names 0 "<#_ Select role... #>"]
+    set possible_user_role_names [linsert $possible_user_role_names 0 "[_ imsld.Select_role]"]
     set user_role_id -1
 }
 
