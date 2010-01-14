46dccd8c9f386c13dae73c14542b5c67ed2621c2
Emmanuelle Raffenne <eraffenne@gmail.com>
2010-01-14 10:31:02 +0000

    untabify and indent

--- tcl/apm-callback-procs.tcl
+++ tcl/apm-callback-procs.tcl
@@ -44,7 +44,7 @@ ad_proc -public imsld::apm_callback::before_unmount {
     -node_id:required
     -package_id:required
 } {
-     task that must be done before unmount the imsld package 
+    task that must be done before unmount the imsld package 
 } { 
 
 
@@ -87,27 +87,27 @@ ad_proc -public imsld::apm_callback::after_upgrade {
     @error 
 } {
     apm_upgrade_logic \
-	-from_version_name $from_version_name \
-	-to_version_name $to_version_name \
-	-spec {
-	    1.3d 1.4d {
-		content::type::attribute::new -content_type imsld_complete_act \
-		    -attribute_name time_string -datatype string \
-		    -pretty_name "#imsld.Time_String#" -column_spec "varchar(30)"
-	    }
-	    1.5d 1.6d {
-		content::type::attribute::new -content_type imsld_imsld \
-		    -attribute_name resource_handler -datatype string \
-		    -pretty_name "#imsld.Resource_Handler#" -column_spec "varchar(100)"
-	    }
-	    1.7d 1.8d {
-		imsld::gsi::install::do_install
-	    }
-	    2.0d 2.0 {
-		if { [imsld::global_folder_id] eq "" } {
-		    imsld::install::create_global_folder
-		}
-	    }
-	}
+        -from_version_name $from_version_name \
+        -to_version_name $to_version_name \
+        -spec {
+            1.3d 1.4d {
+                content::type::attribute::new -content_type imsld_complete_act \
+                    -attribute_name time_string -datatype string \
+                    -pretty_name "#imsld.Time_String#" -column_spec "varchar(30)"
+            }
+            1.5d 1.6d {
+                content::type::attribute::new -content_type imsld_imsld \
+                    -attribute_name resource_handler -datatype string \
+                    -pretty_name "#imsld.Resource_Handler#" -column_spec "varchar(100)"
+            }
+            1.7d 1.8d {
+                imsld::gsi::install::do_install
+            }
+            2.0d 2.0 {
+                if { [imsld::global_folder_id] eq "" } {
+                    imsld::install::create_global_folder
+                }
+            }
+        }
 
 }
